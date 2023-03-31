7e90dc3b95e18daa86c0a6538d315365d917ec99
偉林 <1632247699@qq.com>
2023-03-31 14:11:19 +0800

    first commit

---
+++ wuweilinMFC/wuweilinMFC/wuweilinMFCDlg.h
@@ -0,0 +1,34 @@
+﻿
+// wuweilinMFCDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CwuweilinMFCDlg 对话框
+class CwuweilinMFCDlg : public CDialogEx
+{
+// 构造
+public:
+	CwuweilinMFCDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WUWEILINMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
